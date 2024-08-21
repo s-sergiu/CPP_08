fd0282f2156e8baab8883447de00bec7d6c9eacb
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-22 00:58:32 +0200

    added simple implementations

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,11 @@
+
+#ifndef EASY_HPP
+#define EASY_HPP
+
+
+template<typename T>
+T easyfind (T &arg1, int &arg2);
+
+#include "easyfind.tpp"
+
+#endif
---
+++ ex00/easyfind.tpp
@@ -0,0 +1,6 @@
+
+template<typename T>
+T easyfind (T &arg1, int &arg2) {
+	(void)arg1;
+	(void)arg2;
+}
