1938111c72e5d137eb9ef2f8b5b224040f966ce7
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-22 17:51:46 +0200

    Added libs

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -2,9 +2,12 @@
 #ifndef EASY_HPP
 #define EASY_HPP
 
+#include <algorithm>
+#include <vector>
+#include <iostream>
 
 template<typename T>
-T easyfind (T &arg1, int &arg2);
+T easyfind (T &arg1, int arg2);
 
 #include "easyfind.tpp"
 
