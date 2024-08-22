ba1e3c6abae7526d88897df4aa5b2344f8b7b0f2
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-22 21:53:59 +0200

    updated main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,13 +1,20 @@
 
 #include "easyfind.hpp"
+#include <vector>
+#include <set>
 
 int main(void) {
 	
-	int container[] = {37, 42, 69};
-	try {
-		int it;
+	std::vector<int> vec;
+	std::vector<int>::iterator it;
 
-		it = easyfind(container, container + 4, 69);
+	vec.push_back(10);
+	vec.push_back(20);
+	vec.push_back(69);
+	vec.push_back(30);
+	try {
+		it = easyfind(vec, 69);
+		std::cout<<vec.end() - it<<std::endl;
 	} catch (std::exception &e) {
 		std::cerr<<"Error: "<<e.what()<<std::endl;
 	}
