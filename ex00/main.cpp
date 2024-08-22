406e0251466c6bb17d61805a77523ab863acea14
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-22 17:52:26 +0200

    updated main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,5 +3,12 @@
 
 int main(void) {
 	
-	return 0;
+	int container[] = {37, 42, 69};
+	try {
+		int it;
+
+		it = easyfind(container, container + 4, 69);
+	} catch (std::exception &e) {
+		std::cerr<<"Error: "<<e.what()<<std::endl;
+	}
 }
