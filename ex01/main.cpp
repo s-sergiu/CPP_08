2f1b24261edd2e8a9013fe542a2bb0c78261bd1a
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-23 20:21:50 +0200

    updated main file

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,12 +2,21 @@
 #include "Span.hpp"
 
 int main(void) {
-	Span obj(2);
+	Span obj(10);
 
 	try {
 		obj.addNumber(4);
+		obj.addNumber(4);
 		obj.addNumber(2);
 		obj.addNumber(3);
+		obj.addNumber(23);
+		obj.addNumber(103);
+		obj.addNumber(42);
+		obj.addNumber(31);
+		obj.addNumber(29);
+		obj.addNumber(78);
+		std::cout<<obj.longestSpan()<<std::endl;
+		std::cout<<obj.shortestSpan()<<std::endl;
 	} catch (std::exception &e) {
 		std::cerr<<"Exception: "<<e.what()<<std::endl;
 	}
