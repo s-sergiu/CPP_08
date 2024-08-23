a54ff00552a7e045ea839a1f8ef0cee896300cfa
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-23 12:03:35 +0200

    updated main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,6 +2,14 @@
 #include "Span.hpp"
 
 int main(void) {
+	Span obj(2);
 
+	try {
+		obj.addNumber(4);
+		obj.addNumber(2);
+		obj.addNumber(3);
+	} catch (std::exception &e) {
+		std::cerr<<"Exception: "<<e.what()<<std::endl;
+	}
 	return 0;
 }
