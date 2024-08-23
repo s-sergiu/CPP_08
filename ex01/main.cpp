384585765972361a4e5d0fe76f26699bb31bba67
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-23 20:57:12 +0200

    updated main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,6 +3,12 @@
 
 int main(void) {
 	Span obj(10);
+	std::vector<int> vec;
+
+	vec.push_back(10);
+	vec.push_back(20);
+	vec.push_back(30);
+	vec.push_back(40);
 
 	try {
 		obj.addNumber(23);
@@ -11,6 +17,7 @@ int main(void) {
 		obj.addNumber(31);
 		obj.addNumber(29);
 		obj.addNumber(78);
+		//obj.addNumbersByRange(vec.begin(), vec.end());
 		std::cout<<obj.longestSpan()<<std::endl;
 		std::cout<<obj.shortestSpan()<<std::endl;
 	} catch (std::exception &e) {
