f0f9a891abbd6923b60805a8376de90d12b86fc2
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-25 16:25:47 +0200

    Implemented derived class mutantstack

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -2,7 +2,19 @@
 #ifndef MUTANT_HPP
 #define MUTANT_HPP
 
+#include <stack>
 
-#include "MutantStack.tpp"
+template<class T, class Container = std::stack<T> >
+class MutantStack : public Container { 
+	public:
+		typedef typename Container::container_type::iterator iterator;
+		typedef typename Container::container_type::const_iterator const_iterator;
+		iterator begin(void) { return this->c.begin(); };
+		iterator end(void) { return this->c.end(); };
+		const_iterator c_begin(void) { return this->c.c_begin(); };
+		const_iterator c_end(void) { return this->c.c_end(); };
+};
+
+//#include "MutantStack.tpp"
 
 #endif
